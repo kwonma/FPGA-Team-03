3c07e14aa7de644159961a50976d5b58838cd8c3
steinbez <32185492+steinbez@users.noreply.github.com>
2018-06-02 10:20:10 -0700

    Add files via upload

---
+++ mux.sv
@@ -0,0 +1,17 @@
+module multiplexer(
+	input [3:0] key_mux,
+	input [3:0] ir_mux,
+	input [3:0] b_mux,
+	input [1:0] dip,
+	output logic [8:0] mux_en
+	);
+	
+always_comb	begin
+		case(dip)
+			0:		mux_en = key_mux;
+			1:		mux_en = ir_mux;
+			2:		mux_en = b_mux;
+		endcase
+	end
+
+endmodule
