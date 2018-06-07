61a4ef246e6de9f4e440ecf77a4d631c1b79aeb0
steinbez <32185492+steinbez@users.noreply.github.com>
2018-06-06 17:08:50 -0700

    Add files via upload

---
+++ encoder.sv
@@ -0,0 +1,21 @@
+module snes_encoder (
+	input logic clock,
+	input logic reset,
+	input logic load,
+	input logic [N-1:0] d,
+	output logic snes_output
+	);
+
+logic[N-1:0] q;
+
+always_ff(posedge clk, posedge reset)
+	if (reset)
+		q <= 0;
+	else if (load)
+		q <= d;
+	else
+		q <= {1, q[N-1:1]};
+		
+assign snes_out = q[0];
+
+endmodule
